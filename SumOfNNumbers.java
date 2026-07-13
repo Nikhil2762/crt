5893c2dd796bf6101aea5f528eb9e91c534e3add
AnneMukeshBalaji <mukimukku@gmail.com>
2026-07-13 10:38:29 +0530

    made some changes to SumOfNNumbers file :

--- SumOfNNumbers.java
+++ SumOfNNumbers.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class SumOfNNumbers {
   public static void main(String[] args) {
-    int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    Scanner scanner = new Scanner(System.in);
+    // int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int[] arr = new int[10];
+    for (int i = 0; i < 10; i++) {
+      arr[i] = scanner.nextInt();
+    }
+
     int sum = 0;
     for (int value : arr) {
       sum += value;
     }
     System.out.println(sum);
+    scanner.close();
   }
 }
