8f6188d5ae10e24901e2d98dbec32293f234a151
AnneMukeshBalaji <mukimukku@gmail.com>
2026-07-18 11:19:14 +0530

    added second index

--- LinkedList.java
+++ LinkedList.java
@@ -3,6 +3,7 @@ public class LinkedList {
     LinkedNode head = new LinkedNode(-1);
 
     int i = 1;
+    // NOTE: Adding 10 Elements
     while (i <= 10) {
       LinkedNode newNode = new LinkedNode(i * 10);
       LinkedNode current = head;
@@ -18,10 +19,27 @@ public class LinkedList {
       System.out.println(current.value);
       current = current.pointer;
     }
-    System.out.println("Adding At The Start");
+    // NOTE: Adding Element At The Beginning
     LinkedNode addNewFirstElement = new LinkedNode(1000);
     addNewFirstElement.pointer = head.pointer;
     head.pointer = addNewFirstElement;
+    current = head.pointer;
+    while (current != null) {
+      System.out.println(current.value);
+      current = current.pointer;
+    }
+    // NOTE: Adding Element At the Second Place
+    System.out.println("Adding Element At the Second Place");
+    current = head;
+    while (current != null) {
+      if (current.value == 1000 && current.pointer.value == 10) {
+        LinkedNode newNode = new LinkedNode(2000);
+        newNode.pointer = current.pointer;
+        current.pointer = newNode;
+        break;
+      }
+      current = current.pointer;
+    }
     current = head.pointer;
     while (current != null) {
       System.out.println(current.value);
