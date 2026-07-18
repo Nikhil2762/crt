19c6cf5c0349bb0a4348fa61935af81e381a041f
AnneMukeshBalaji <mukimukku@gmail.com>
2026-07-18 14:44:01 +0530

    refactored the code

--- LinkedList.java
+++ LinkedList.java
@@ -1,110 +1,118 @@
 public class LinkedList {
+
   public static void main(String[] args) {
     LinkedNode head = new LinkedNode(-1);
 
-    int i = 1;
-    // NOTE: Adding 10 Elements
-    while (i <= 10) {
-      LinkedNode newNode = new LinkedNode(i * 10);
-      LinkedNode current = head;
-      while (current.pointer != null) {
-        current = current.pointer;
-      }
-      current.pointer = newNode;
-      i++;
+    for (int i = 1; i <= 10; i++) {
+      append(head, i * 10);
     }
 
+    System.out.println("Initial list:");
+    display(head);
+
+    addFirst(head, 1000);
+    System.out.println("After adding 1000 at the beginning:");
+    display(head);
+
+    System.out.println("Adding 2000 at the second place:");
+    insertAfter(head, 1000, 2000);
+    display(head);
+
+    System.out.println("Inserting 69 at index 5:");
+    insertAt(head, 5, 69);
+    display(head);
+
+    deleteFirst(head);
+    System.out.println("After deleting first element:");
+    display(head);
+
+    deleteLast(head);
+    System.out.println("After deleting last element:");
+    display(head);
+
+    deleteByValue(head, 50);
+    System.out.println("After deleting value 50:");
+    display(head);
+  }
+
+  private static void append(LinkedNode head, int value) {
+    LinkedNode newNode = new LinkedNode(value);
+    LinkedNode current = head;
+    while (current.pointer != null) {
+      current = current.pointer;
+    }
+    current.pointer = newNode;
+  }
+
+  private static void display(LinkedNode head) {
     LinkedNode current = head.pointer;
     while (current != null) {
       System.out.println(current.value);
       current = current.pointer;
     }
-    // NOTE: Adding Element At The Beginning
-    LinkedNode addNewFirstElement = new LinkedNode(1000);
-    addNewFirstElement.pointer = head.pointer;
-    head.pointer = addNewFirstElement;
-    current = head.pointer;
+  }
+
+  private static void addFirst(LinkedNode head, int value) {
+    LinkedNode newNode = new LinkedNode(value);
+    newNode.pointer = head.pointer;
+    head.pointer = newNode;
+  }
+
+  private static boolean insertAfter(LinkedNode head, int targetValue, int newValue) {
+    LinkedNode current = head;
     while (current != null) {
-      System.out.println(current.value);
-      current = current.pointer;
-    }
-    // NOTE: Adding Element At the Second Place
-    System.out.println("Adding Element At the Second Place");
-    current = head;
-    while (current != null) {
-      if (current.value == 1000 && current.pointer.value == 10) {
-        LinkedNode newNode = new LinkedNode(2000);
+      if (current.value == targetValue && current.pointer != null) {
+        LinkedNode newNode = new LinkedNode(newValue);
         newNode.pointer = current.pointer;
         current.pointer = newNode;
-        break;
+        return true;
       }
       current = current.pointer;
     }
-    current = head.pointer;
-    while (current != null) {
-      System.out.println(current.value);
-      current = current.pointer;
-    }
-    current = head;
-    LinkedNode next = current.pointer;
-    i = 1;
-    // NOTE: Inserting At the Fifth Place
-    System.out.println("Adding Element At the Fifth Index ");
-    while (current != null) {
-      if (i == 5) {
-        LinkedNode newNode = new LinkedNode(69);
+    return false;
+  }
+
+  private static boolean insertAt(LinkedNode head, int index, int value) {
+    LinkedNode current = head;
+    int i = 1;
+    while (current != null && current.pointer != null) {
+      if (i == index) {
+        LinkedNode newNode = new LinkedNode(value);
+        newNode.pointer = current.pointer;
         current.pointer = newNode;
-        newNode.pointer = next;
-        break;
+        return true;
       }
       current = current.pointer;
-      next = current.pointer;
-      i += 1;
+      i++;
     }
-    current = head.pointer;
-    while (current != null) {
-      System.out.println(current.value);
+    return false;
+  }
+
+  private static void deleteFirst(LinkedNode head) {
+    if (head.pointer != null) {
+      head.pointer = head.pointer.pointer;
+    }
+  }
+
+  private static void deleteLast(LinkedNode head) {
+    LinkedNode current = head;
+    if (current.pointer == null) return;
+
+    while (current.pointer.pointer != null) {
       current = current.pointer;
     }
-    // NOTE: Deleting At The First Position
-    head.pointer = head.pointer.pointer;
-    current = head.pointer;
-    System.out.println("Deleting First Element");
-    while (current != null) {
-      System.out.println(current.value);
-      current = current.pointer;
-    }
-    // NOTE: Deleting the last element
-    current = head;
-    LinkedNode previous = null;
-    System.out.println("Deleting The Last Element");
+    current.pointer = null;
+  }
+
+  private static boolean deleteByValue(LinkedNode head, int value) {
+    LinkedNode current = head;
     while (current.pointer != null) {
-      previous = current;
+      if (current.pointer.value == value) {
+        current.pointer = current.pointer.pointer;
+        return true;
+      }
       current = current.pointer;
     }
-    previous.pointer = null;
-
-    current = head.pointer;
-    while (current != null) {
-      System.out.println(current.value);
-      current = current.pointer;
-    }
-
-    // NOTE: Deleting a Specific Value ;
-    current = head;
-    previous = null;
-    System.out.println("Deleting The Value 50");
-    while(current != null){
-        if(current.value == 50){
-            previous.pointer = current.pointer;
-        }
-        previous = current;
-        current = current.pointer;
-    }
-    current = head.pointer;
-    while(current != null){
-        System.out.println(current.value);
-        current = current.pointer;
-    }
+    return false;
   }
 }
