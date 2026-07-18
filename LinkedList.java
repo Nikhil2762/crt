290ad836951241d24af17b618d9245e905c68077
AnneMukeshBalaji <mukimukku@gmail.com>
2026-07-18 10:50:55 +0530

    added new linked list modified files

--- LinkedList.java
+++ LinkedList.java
@@ -1,8 +1,31 @@
 public class LinkedList {
   public static void main(String[] args) {
     LinkedNode head = new LinkedNode(-1);
-    LinkedNode newNode = new LinkedNode(10);
-    head.pointer = newNode;
-    System.out.println(head.pointer.value);
+
+    int i = 1;
+    while (i <= 10) {
+      LinkedNode newNode = new LinkedNode(i * 10);
+      LinkedNode current = head;
+      while (current.pointer != null) {
+        current = current.pointer;
+      }
+      current.pointer = newNode;
+      i++;
+    }
+
+    LinkedNode current = head.pointer;
+    while (current != null) {
+      System.out.println(current.value);
+      current = current.pointer;
+    }
+    System.out.println("Adding At The Start");
+    LinkedNode addNewFirstElement = new LinkedNode(1000);
+    addNewFirstElement.pointer = head.pointer;
+    head.pointer = addNewFirstElement;
+    current = head.pointer;
+    while (current != null) {
+      System.out.println(current.value);
+      current = current.pointer;
+    }
   }
 }
