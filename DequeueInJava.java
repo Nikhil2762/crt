22a6b68ff27853ec37ff79d9ba199f5d4d5678bc
AnneMukeshBalaji <mukimukku@gmail.com>
2026-07-21 10:10:59 +0530

    added dequeue

---
+++ DequeueInJava.java
@@ -0,0 +1,28 @@
+public class DequeueInJava {
+  static int [] arr = new int[10];
+  static int head = 0;
+  static int tail = 0;
+  static int size = arr.length ;
+  public static void main(String[] args) {
+    addValue(10); 
+    addValue(20); 
+    addValue(30); 
+    addValue(40); 
+    display();
+
+  }
+  public static void addValue(int val){
+    if(tail == size - 1){
+      System.out.println("OverFlow Errro");
+      return;
+    }
+    arr[tail] = val;
+    tail += 1;
+  }
+  public static void display(){
+    if(tail == head)return;
+    for(int i = head;i<tail;i++){
+      System.out.println(arr[i]);
+    }
+  }
+}
