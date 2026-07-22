12de445fd2e57affa45901d8c00e03e3564caba3
AnneMukeshBalaji <mukimukku@gmail.com>
2026-07-22 11:59:15 +0530

    added tree node file

---
+++ BinaryTree.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class BinaryTree {
+  public static TreeNode createNode(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter data (-1) for NULL: ");
+    int data = scanner.nextInt();
+    TreeNode root = new TreeNode(data);
+    System.out.println("Enter Data for Left Child: ");
+    root.left = createNode();
+    System.out.println("Enter Data for Right Child: ");
+    root.right= createNode();
+    scanner.close();
+    return root;
+  }
+}
